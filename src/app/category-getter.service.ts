73c96d4353b27f4d54d806887e4c636901e6c457
Jonathan <jms15af@my.fsu.edu>
2021-07-21 23:48:01 -0400

    Revert "compile app for pages"
    This reverts commit 675207cb9835fdae7c2e517c9ed439ca85dd0077.

--- src/app/category-getter.service.ts
+++ src/app/category-getter.service.ts
@@ -8,7 +8,7 @@ import { Clue } from './clue';
   providedIn: 'root'
 })
 export class CategoryGetterService {
-  private readonly USE_FAKE_CATEGORY = false;
+  private readonly USE_FAKE_CATEGORY = true;
   private readonly FAKE_CATEGORY_JS =
     '{"id": 55,"title": "sport <i>of</i> kings","clues_count": 25,"clues": [{"id": 303,"answer": "bet on horses","question": "The <i>pari-mutuel</i> system lets you do it legally","value": 100,"airdate": "1984-11-29T12:00:00.000Z","category_id": 55,"game_id": null,"invalid_count": null},{"id": 309,"answer": "a handicap","question": "Race where faster horses carry more weight than slower ones","value": 200,"airdate": "1984-11-29T12:00:00.000Z","category_id": 55,"game_id": null,"invalid_count": null},{"id": 315,"answer": "a mudder","question": "Race horse that runs well on a wet track, or a Bronx mommy","value": 300,"airdate": "1984-11-29T12:00:00.000Z","category_id": 55,"game_id": null,"invalid_count": null},{"id": 327,"answer": "J. Paul Getty","question": "Question was missing!","value": null,"airdate": "1984-11-29T12:00:00.000Z","category_id": 55,"game_id": null,"invalid_count": null},{"id": 321,"answer": "Adam Clayton Powell","question": "Question was missing!","value": null,"airdate": "1984-11-29T12:00:00.000Z","category_id": 55,"game_id": null,"invalid_count": 1}]}';
 
