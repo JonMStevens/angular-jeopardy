615c5f090c04acd04ce6bba53e48ccfbfb614678
Jonathan <jms15af@my.fsu.edu>
2021-08-11 17:44:33 -0400

    add game mode bool set by menu buttons

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GameStateService } from '../game-state.service';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private playerService: PlayerService,
+    private gameStateService: GameStateService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  navToGame(): void {
+    this.gameStateService.inGameMode = true;
+    this.gameStateService.reset();
+    this.playerService.reset();
+    this.router.navigate(['/game']);
   }
-
+  navToCoryat(): void {
+    this.gameStateService.inGameMode = false;
+    this.gameStateService.reset();
+    this.playerService.reset();
+    this.router.navigate(['/coryat']);
+  }
 }
