14ee3e96e3309596765f9f0d8174542521238c44
Jonathan <jms15af@my.fsu.edu>
2021-08-11 22:30:55 -0400

    prevent going to wrong component in the wrong mode

--- src/app/coryat/coryat-board/coryat-board.component.ts
+++ src/app/coryat/coryat-board/coryat-board.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GameStateService } from 'src/app/game-state.service';
 
 @Component({
   selector: 'app-coryat-board',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coryat-board.component.css']
 })
 export class CoryatBoardComponent implements OnInit {
-
-  constructor() { }
+  constructor(private gameState: GameStateService, private router: Router) {}
 
   ngOnInit(): void {
+    /* todo: is there a way to do this in router module or if not maybe import router only when needed? */
+    if (!this.gameState.inCoryatMode) {
+      this.router.navigate(['/menu']);
+    }
   }
-
 }
