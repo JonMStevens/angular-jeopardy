a48570313a6f2cc674e1a536aff0ea40c8c748fe
Jonathan <jms15af@my.fsu.edu>
2021-08-14 18:12:31 -0400

    use route guards to prevent wrong mode access

---
+++ src/app/guards/coryat-mode.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { GameStateService } from '../game-state.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoryatModeGuard implements CanActivate {
+  constructor(private gameState: GameStateService, private router: Router) {}
+
+  canActivate():
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    /* if in game mode return true */
+    if (this.gameState.inCoryatMode) {
+      return true;
+    }
+
+    return this.router.parseUrl('/menu');
+  }
+}
