a6d0de63e1d9c3baff7521dbe1b4ec4e892fd3fd
Jonathan <jms15af@my.fsu.edu>
2021-08-16 20:25:58 -0400

    transition from coryat setup to board with buttons

--- src/app/coryat/coryat-board/coryat-board.component.ts
+++ src/app/coryat/coryat-board/coryat-board.component.ts
@@ -10,6 +10,19 @@ import { GameStateService } from 'src/app/game-state.service';
 export class CoryatBoardComponent implements OnInit {
   constructor(private gameState: GameStateService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    document.body.style.overflowY = 'hidden';
+  }
+  ngOnDestroy(): void {
+    document.body.style.overflowY = 'unset';
+  }
   showBoard = (): boolean => this.gameState.currentClue == null;
+  screenScroll(id: string): void {
+    if (!id) return;
+
+    const element = document.querySelector('#' + id);
+    if (!element) return;
+
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
 }
