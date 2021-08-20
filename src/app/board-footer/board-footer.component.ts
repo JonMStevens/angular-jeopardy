228f71df92e8344a1be6d0d76b90891e4d476612
Jonathan <jms15af@my.fsu.edu>
2021-08-20 00:23:09 -0400

    use host context, not extra property on footer

--- src/app/board-footer/board-footer.component.ts
+++ src/app/board-footer/board-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faUserPlus, faUserMinus } from '@fortawesome/free-solid-svg-icons';
 import { GameStateService } from '../game-state.service';
 import { PlayerService } from '../player.service';
@@ -12,16 +12,12 @@ export class BoardFooterComponent implements OnInit {
   faUserPlus = faUserPlus;
   faUserMinus = faUserMinus;
   showingDoubleJeopardyAlert = false;
-  @Input() isHorizontal: boolean | undefined = undefined;
-  @HostBinding('class.vertical') applyVerticalClass = false;
 
   constructor(
     private gameState: GameStateService,
     public playerService: PlayerService
   ) {}
-  ngOnInit(): void {
-    this.applyVerticalClass = this.isHorizontal === false;
-  }
+  ngOnInit(): void {}
   showDoubleJeopardyButton = (): boolean => this.gameState.round == 1;
   startDoubleJeopardy(): void {
     this.hideDoubleJeopardyAlert();
@@ -36,7 +32,6 @@ export class BoardFooterComponent implements OnInit {
   }
 
   arrangePlayersVertically(): boolean {
-    if (!this.isHorizontal) return true;
     return this.playerService.players.length >= 4;
   }
 }
