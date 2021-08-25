1dd986f23bf35334f633ebe4d7a6f441f8264f9b
Jonathan <jms15af@my.fsu.edu>
2021-08-25 03:28:31 -0400

    add basic content to coyat info page

--- src/app/coryat/coryat-info/coryat-info.component.ts
+++ src/app/coryat/coryat-info/coryat-info.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Player } from 'src/app/player';
 @Component({
   selector: 'app-coryat-info',
   templateUrl: './coryat-info.component.html',
   styleUrls: ['./coryat-info.component.css']
 })
 export class CoryatInfoComponent implements OnInit {
+  demoPlayer: Player = { number: 0, name: 'Player 1', score: 0 };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
